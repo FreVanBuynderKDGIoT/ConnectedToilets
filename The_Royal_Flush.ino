8283fb9ab19c86df43a0d09302307214ee76e8e1
FreVanBuynderKDGIoT <74856561+FreVanBuynderKDGIoT@users.noreply.github.com>
2021-01-14 10:45:20 +0100

    Add files via upload

--- The_Royal_Flush.ino
+++ The_Royal_Flush.ino
@@ -1,22 +1,16 @@
 #include "config.h"
 #include "Chrono.h"
+#include <ezButton.h>
 
-#define btnToilet1 1
-#define btnToilet2 2
-#define btnToilet3 3
+ezButton btnToilet1(4); 
+ezButton btnToilet2(5); 
+ezButton btnToilet3(2);
 
 //This sensors normally go into an analog pin
 //Because the huzzah feather only has 1 analog pin, are we connecting these to a digital pin with INPUT_PULLUP
 #define dstncPiscine1 4
 #define dstncPiscine2 5
-#define dstncPiscine3 6
-
-bool stateToilet1;
-bool stateToilet2;
-bool stateToilet3;
-bool statePiscine1;
-bool statePiscine2;
-bool statePiscine3;
+#define dstncPiscine3 2
 
 int cntToilet1 = 0;
 int cntToilet2 = 0;
@@ -25,13 +19,12 @@ int cntPiscine1 = 0;
 int cntPiscine2 = 0;
 int cntPiscine3 = 0;
 
-AdafruitIO_Feed *toilet1 = io.feed("Toilet 1");
-AdafruitIO_Feed *toilet2 = io.feed("Toilet 2");
-AdafruitIO_Feed *toilet3 = io.feed("Toilet 3");
-AdafruitIO_Feed *piscine1 = io.feed("Piscine 1");
-AdafruitIO_Feed *piscine2 = io.feed("Piscine 2");
-AdafruitIO_Feed *piscine3 = io.feed("Piscine 3");
+int reading;
 
+AdafruitIO_Group *group = io.group("Connected Toilets");
+
+
+Chrono sendTimer;
 Chrono tmPiscine1;
 Chrono tmPiscine2;
 Chrono tmPiscine3;
@@ -39,7 +32,7 @@ Chrono tmPiscine3;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // waiting for serial monitor to open
   while (! Serial);
@@ -47,6 +40,10 @@ void setup()
   Serial.print("Connecting to Adafruit IO");
 
   io.connect();
+
+  //group->onMessage("Toilet 1", handleToilet1);
+  //group->onMessage("Toilet 2", handleToilet2);
+  //group->onMessage("Toilet 3", handleToilet3);
 
   // Wait for a connection with the cloud
   while(io.status() < AIO_CONNECTED)
@@ -57,22 +54,11 @@ void setup()
 
   Serial.println();
   Serial.println(io.statusText());
-
-  pinMode(btnToilet1, INPUT_PULLUP);
-  pinMode(btnToilet2, INPUT_PULLUP);
-  pinMode(btnToilet3, INPUT_PULLUP);
-
-  // This pins are INPUT_PULLUP but must be INPUT if on an analog pin
-  pinMode(dstncPiscine1, INPUT_PULLUP);
-  pinMode(dstncPiscine2, INPUT_PULLUP);
-  pinMode(dstncPiscine3, INPUT_PULLUP);
-
-  toilet1->onMessage(handleToilet1);
-  toilet2->onMessage(handleToilet2);
-  toilet3->onMessage(handleToilet3);
-  piscine1->onMessage(handlePiscine1);
-  piscine2->onMessage(handlePiscine2);
-  piscine3->onMessage(handlePiscine3);
+  btnToilet1.setCountMode(COUNT_FALLING);
+  btnToilet2.setCountMode(COUNT_FALLING);
+  btnToilet3.setCountMode(COUNT_FALLING);
+  
+  group->get();
 }
 
 void loop() 
@@ -80,40 +66,43 @@ void loop()
   // this line is possible in the if statements, we will try if this works
   io.run(); // keeps the client connected
 
-  stateToilet1 = digitalRead(btnToilet1);
-  stateToilet2 = digitalRead(btnToilet2);
-  stateToilet3 = digitalRead(btnToilet3);
+  btnToilet1.loop();
+  btnToilet2.loop();
+  btnToilet3.loop();
+  
+  cntToilet1 = btnToilet1.getCount();
+  cntToilet2 = btnToilet2.getCount();
+  cntToilet3 = btnToilet3.getCount();
 
   //normally on digital pins we use digitalRead(), we use analogRead() because we don't have to change the code later on
-  statePiscine1 = analogRead(dstncPiscine1);
-  statePiscine2 = analogRead(dstncPiscine2);
-  statePiscine3 = analogRead(dstncPiscine3);
+  //statePiscine1 = analogRead(dstncPiscine1);
+  //statePiscine2 = analogRead(dstncPiscine2);
+  //statePiscine3 = analogRead(dstncPiscine3);
 
-  if (stateToilet1 == true)
-  {
-    cntToilet1++;
-    
-    Serial.print("sending -> ");
+
+  if (btnToilet1.isPressed())
+  { 
+    Serial.print("sending T1 -> ");
     Serial.println(cntToilet1);
-    toilet1->save(cntToilet1);
+    group->set("Toilet 1", cntToilet1);
+    group->save();
   }
-  if (stateToilet2 == true)
+  if (btnToilet2.isPressed())
   {
-    cntToilet2++;
-    
-    Serial.print("sending -> ");
+    Serial.print("sending T2 -> ");
     Serial.println(cntToilet2);
-    toilet2->save(cntToilet2);
+    group->set("Toilet 2", cntToilet2);
+    group->save();
   }
-  if (stateToilet3 == true)
+  if (btnToilet3.isPressed())
   {
-    cntToilet3++;
-    
-    Serial.print("sending -> ");
+    Serial.print("sending T3 -> ");
     Serial.println(cntToilet3);
-    toilet3->save(cntToilet3);
+    group->set("Toilet 3", cntToilet3);
+    group->save();
   }
-  
+}
+/*
   if (statePiscine1 >= 800 && tmPiscine1.hasPassed(10000))
   {
     tmPiscine1.stop();
@@ -153,31 +142,31 @@ void loop()
     tmPiscine3.restart();
   }
 }
-
+*/
 void handleToilet1(AdafruitIO_Data *data)
 {
-  int reading = data->toInt();
+  reading = data->toInt();
   Serial.print("received <- ");
   Serial.println(reading);
-  analogWrite(cntToilet1, reading);
+  cntToilet1 = reading;
 }
 
 void handleToilet2(AdafruitIO_Data *data)
 {
-  int reading = data->toInt();
+  reading = data->toInt();
   Serial.print("received <- ");
   Serial.println(reading);
-  analogWrite(cntToilet2, reading);
+  cntToilet2 = reading;
 }
 
 void handleToilet3(AdafruitIO_Data *data)
 {
-  int reading = data->toInt();
+  reading = data->toInt();
   Serial.print("received <- ");
   Serial.println(reading);
-  analogWrite(cntToilet3, reading);
+  cntToilet3 = reading;
 }
-
+/*
 void handlePiscine1(AdafruitIO_Data *data)
 {
   int reading = data->toInt();
@@ -201,3 +190,4 @@ void handlePiscine3(AdafruitIO_Data *data)
   Serial.println(reading);
   analogWrite(cntPiscine3, reading);
 }
+*/
